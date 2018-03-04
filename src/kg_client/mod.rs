160e980d20ce01c5441179e9904eb90a225282e7
Anthony Weston <anthonyelliotweston@gmail.com>
2018-03-04 17:31:57 -0500

    uri generation from Request struct

--- src/kg_client/mod.rs
+++ src/kg_client/mod.rs
@@ -8,8 +8,9 @@ use self::hyper::net::HttpsConnector;
 use self::hyper_native_tls::NativeTlsClient;
 
 pub struct GoogleGraphClient {
+	client: Client,
 	base_url: String,
-	client: Client
+	api_key: String
 }
 
 impl GoogleGraphClient {
@@ -19,37 +20,38 @@ impl GoogleGraphClient {
 		let connector = HttpsConnector::new(ssl);
 		let client = Client::with_connector(connector);
 
-		return GoogleGraphClient{client: client, base_url: String::from("https://kgsearch.googleapis.com/v1/entities:search?")};
+		let mut filename = "src/kg_client/api_key.txt".to_string();
+		let mut f = File::open(filename).expect("file not found");
+		let mut api_key = String::new();
+		f.read_to_string(&mut api_key).expect("something went wrong reading the file");
+
+		return GoogleGraphClient{client: client, base_url: String::from("https://kgsearch.googleapis.com/v1/entities:search?"), api_key: api_key};
 	}
 
-	pub fn make_request(&self) {
+	pub fn make_request(&self, request: &Request) {
+		let mut target_uri = self.base_url.clone();
+		self.construct_url(request, &mut target_uri);
 
 		let mut s = String::new();
-
-		let mut api_key = self.get_api_key();
-
-		let mut target_uri = &self.base_url;
-
-	    let mut res = self.client.get(target_uri).send()
-	                    .unwrap()
-	                    .read_to_string(&mut s)
-	                    .unwrap();
+	    let mut res = self.client.get(&target_uri).send().unwrap().read_to_string(&mut s).unwrap();
 		
     	println!("Result: {}", s);
 	}
 	
-	fn get_api_key(&self) -> String{
-		let mut filename = "src/kg_client/api_key.txt".to_string();
-		let mut f = File::open(filename).expect("file not found");
+	fn construct_url(&self, request: &Request, s: &mut String){
+		
+		let temp = str::replace(&request.query, " ", "+");
+		s.push_str("query=");
+		s.push_str(&temp);
 
-		let mut contents = String::new();
-		f.read_to_string(&mut contents).expect("something went wrong reading the file");
+		s.push_str("&key=");
+		s.push_str(&self.api_key);
 
-		return contents;
-	}
-	
-	fn construct_url(&self, request: &Request){
+		s.push_str("&languages=");
+		s.push_str(&request.languages);
 
+		s.push_str("&limit=");
+		s.push_str(&request.limit.to_string());
 	}
 
 }
@@ -65,7 +67,8 @@ pub struct Request {
 }
 
 impl Request{
-	pub fn new(query: String, limit: i32 ) -> Request{
+	pub fn new(copy_query: String, limit: i32 ) -> Request{
+		let query = copy_query.clone();
 		Request {query: query, ids: None, languages: String::from("en"), types: None, indent: false, prefix: false, limit: limit}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ mod server;
 mod kg_client;
 
 fn main() {
+
+	let query = String::from("Al Green");
+	let request = kg_client::Request::new(query, 1);
 	let mut ggc = kg_client::GoogleGraphClient::new();
-	
-	ggc.make_request();
+
+	ggc.make_request(&request);
 }
