f9c878fcc76e6d5a832dfc705eb7288152885195
Anthony Weston <anthonyelliotweston@gmail.com>
2018-03-04 15:56:40 -0500

    successful connection to google knowledge graph api

--- src/kg_client/mod.rs
+++ src/kg_client/mod.rs
@@ -1,40 +1,57 @@
-extern crate serialize;
-extern crate futures;
 extern crate hyper;
-extern crate tokio_core;
+extern crate hyper_native_tls;
 
-use std::io::{self, Write};
-use std::vec::Vec;
-
-use serialize::json;
-use futures::{Future, Stream};
-use hyper::Client;
-use tokio_core::reactor::Core;
+use std::io::{self, Write, Read};
+use std::fs::File;
+use self::hyper::Client;
+use self::hyper::net::HttpsConnector;
+use self::hyper_native_tls::NativeTlsClient;
 
 pub struct GoogleGraphClient {
-	core: Core,
-	client: Client,
-	pub base_url: String
+	base_url: String,
+	client: Client
 }
 
 impl GoogleGraphClient {
 
-	pub init_client() -> bool {
-		core = Core::new()?;
-		client = Client::new(&core.handle());
+	pub fn new() -> GoogleGraphClient {
+		let ssl = NativeTlsClient::new().unwrap();
+		let connector = HttpsConnector::new(ssl);
+		let client = Client::with_connector(connector);
+
+		return GoogleGraphClient{client: client, base_url: String::from("https://kgsearch.googleapis.com/v1/entities:search?")};
 	}
 
-	pub make_request(r: Request) -> Vec<client::Response> {
-		let work = client.get(uri);
-	}
+	pub fn make_request(&self) {
 
-	get_api_key(){
+		let mut s = String::new();
+
+		let mut api_key = self.get_api_key();
+
+		let mut target_uri = &self.base_url;
+
+	    let mut res = self.client.get(target_uri).send()
+	                    .unwrap()
+	                    .read_to_string(&mut s)
+	                    .unwrap();
 		
+    	println!("Result: {}", s);
+	}
+	
+	fn get_api_key(&self) -> String{
+		let mut filename = "src/kg_client/api_key.txt".to_string();
+		let mut f = File::open(filename).expect("file not found");
+
+		let mut contents = String::new();
+		f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+		return contents;
+	}
+	
+	fn construct_url(&self, request: &Request){
+
 	}
 
-	construct_url(r: Request) -> String{
-
-	}
 }
 
 pub struct Request {
@@ -49,20 +66,17 @@ pub struct Request {
 
 impl Request{
 	pub fn new(query: String, limit: i32 ) -> Request{
-		Request {query: query, languages: String::from("en"), indent: false, prefix: false, limit: limit}
+		Request {query: query, ids: None, languages: String::from("en"), types: None, indent: false, prefix: false, limit: limit}
 	}
 }
 
 
 pub struct Response {
-	@id: String,
+	id: String,
 	name: String,
-	@type: Vec<String>,
+	types: Vec<String>,
 	description: String,
-	image: String, 
-	detailedDescription: string,
-	url: String,
-	resultScore: i32
+	result_score: f64
 }
 
 impl Response {
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-extern crate mio;
-extern crate http_muncher;
-extern crate sha1;
-extern crate rustc_serialize;
 
-use std::collections::HashMap;
-use std::net::SocketAddr;
-
-use mio::*;
-use mio::tcp::*;
 
 mod server;
 mod kg_client;
 
 fn main() {
-	/*
-    let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
-    let server_socket = TcpListener::bind(&address).unwrap();
-
-    let mut event_loop = EventLoop::new().unwrap();
-
-    let mut _server = server::WebSocketServer {
-        token_counter: 1,
-        clients: HashMap::new(),
-        socket: server_socket
-    };
-
-    event_loop.register(&_server.socket,
-                        server::SERVER_TOKEN,
-                        EventSet::readable(),
-                        PollOpt::edge()).unwrap();
-    event_loop.run(&mut _server).unwrap();
-    */
-
-    let mut client = kg_client::GoogleGraphClient{ base_url: String::from("https://kgsearch.googleapis.com/v1/entities:search/")}
-    let request = lg_client::Request::new(String::from("Purple Haze", 10));
-
-    
+	let mut ggc = kg_client::GoogleGraphClient::new();
+	
+	ggc.make_request();
 }
